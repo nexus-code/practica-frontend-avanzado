bf5678da1a51dbafb03951e476946619dbce38e4
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2019-09-16 19:14:01 +0200

    eslint and express (--save) added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,12 @@
 
 window.onscroll = function () {
-        console.log(document.body.scrollTop);
+	console.log(document.body.scrollTop);
 
-    if (document.body.scrollTop > 200 ) {
-        // document.getElementById("myImg").className = "slideUp";
-        console.log(document.body.scrollTop);
-    }
-    
-}
+	if (document.body.scrollTop > 200 ) {
+		// document.getElementById("myImg").className = "slideUp";
+		console.log(document.body.scrollTop);
+	}    
+};
 
 // window.addEventListener('scroll', function () {
 //     console.log('scrolling');
